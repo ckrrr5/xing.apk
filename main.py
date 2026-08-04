3c0391642df2e4876a9ef4a354ff9e87e6c493ee
ckrrr5 <570917272@qq.com>
2026-08-04 11:01:08 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
 
-print("PyShell runtime placeholder")
+class TestApp(App):
+    def build(self):
+        return Label(text="打包成功！")
+
+if __name__ == "__main__":
+    TestApp().run()
